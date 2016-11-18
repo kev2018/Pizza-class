252f5a1981d0b0f53d1e53036bffa11a4eef3629
kev2018 <8kludwig@stpatrick.org>
2016-11-18 13:45:04 -0600

    finished

--- pizza.js
+++ pizza.js
@@ -1,24 +1,28 @@
 class pizza{
   //type constructor below
-constructor(s,mt,vt){
+  constructor(s,mt,vt){
   this.size = s; //array ["s","m","l","xL"]
   this.meatToppings = mt;
   this.veggieToppings = vt;
-}
-sizeCost(){
+  }
+  sizeCost(){
   this.size(S) = 7.99
   this.size(M) = 9.99
-  this.size(L) =2.99
+  this.size(L) =12.99
   this.size(XL) = 15.99
-}
-toppingCost(top){
-  if(top == this.meatToppings){
-    return top.length*.99;
   }
-  if(top == this.veggieToppings){
-    return top.length*.5;
+  toppingCost(top){
+    if(top == this.meatToppings){
+      return top.length*.99;
+    }
+    if(top == this.veggieToppings){
+      return top.length*.5;
+    }
+    price(){
+      return sizeCost + toppingCost[0] + toppingCost[1];
+    }
+    static promotionalDeal(pizza,percent){
+      return  pizza.price()*(1-(percent/100));
+    }
   }
-}
-
-  //type class functions below
 }
